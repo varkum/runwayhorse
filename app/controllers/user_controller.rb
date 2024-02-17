499ff0b82da0cb054887b098418d0d63481da1f2
Varun Kumar <varun.kumar@yale.edu>
2024-02-17 10:58:32 -0500

    first commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,14 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
