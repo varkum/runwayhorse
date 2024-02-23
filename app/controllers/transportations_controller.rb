1f9f86dfe33265a3a786953adcd9e58d78664ef6
Varun Kumar <varun.kumar@yale.edu>
2024-02-22 21:06:47 -0500

    new transport form

--- app/controllers/transportations_controller.rb
+++ app/controllers/transportations_controller.rb
@@ -10,6 +10,10 @@ class TransportationsController < ApplicationController
   
   private
   
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+  
   def transportation_params
     params.require(:transportation).permit(:date, :mode, :notes, :time).with_defaults(booked: true)
   end
