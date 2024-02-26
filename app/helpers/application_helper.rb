bd369fcbf83759d1f7e1f747f7ec301a21b6a1f3
Varun Kumar <varun.kumar@yale.edu>
2024-02-26 16:05:54 -0500

    human time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     date.strftime("%B %e, %Y")
   end
   
+  def human_time(time)
+    Time.new(time).strftime("%l:%M %p")
+  end
+  
   def icon_for(transport_mode, size: '8')
     image_tag "#{transport_mode}.png", class: "h-#{size} w-auto mx-auto mb-2"
   end
