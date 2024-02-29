1e3643cbeeee742c6ed0f8bc161e9ef7e5e898fc
Varun Kumar <varun.kumar@yale.edu>
2024-02-29 13:07:45 -0500

    refactor day header

--- app/helpers/days_helper.rb
+++ app/helpers/days_helper.rb
@@ -1,15 +1,2 @@
 module DaysHelper
-  def show_button_or_value(value:, display_name:, icon:)
-    if value.present?
-      content_tag(:p, class: "h-6 w-auto") do
-				concat(image_tag "#{icon}.svg", class: "h-6 w-auto")
-				concat(content_tag(:span, value, class: "ml-1"))
-      end
-    else
-			link_to edit_day_path, class: "flex items-center justify-center px-4 py-2 text-gray-500" do
-				concat(image_tag("#{icon}.svg", class: "h-6 w-auto mr-1"))
-        concat(content_tag(:span, "Add #{display_name}", class: "text-sm font-light"))
-			end
-    end
-  end
 end
