b391b45374df47d2926aac65b8eacf9ff547bdc6
Varun Kumar <varun.kumar@yale.edu>
2024-03-08 18:50:45 -0500

    day nav

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,12 @@
 class Day < ApplicationRecord
   belongs_to :trip
   has_many :happenings
+  
+  def previous
+    trip.days.find_by(date: date - 1)
+  end
+    
+  def next
+    trip.days.find_by(date: date + 1)
+  end
 end
