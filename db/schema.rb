7ff0af68a56a4ec457e2521358b738ff412556b7
Varun Kumar <varun.kumar@yale.edu>
2024-03-26 16:39:26 -0400

    add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_02_24_033302) do
+ActiveRecord::Schema[7.2].define(version: 2024_03_26_203736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_02_24_033302) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "days", "trips"
