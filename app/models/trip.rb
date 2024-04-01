727a7ce90c24b4f1da37e52069ecaf16f8706951
Varun Kumar <varun.kumar@yale.edu>
2024-04-01 11:09:03 -0400

    jump to today

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -11,6 +11,11 @@ class Trip < ApplicationRecord
     (end_date - start_date).to_i
   end
   
+  def current_day
+    active_day = days.find_by(date: Date.today) if (start_date..end_date).include?(Date.today)
+    active_day || days.first
+  end
+  
   def create_and_setup_days
     save
     (0..length).each do |day_number|
