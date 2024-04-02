af490d4215d6b63901ddc1f698eacf16bb7e1ae9
Varun Kumar <varun.kumar@yale.edu>
2024-04-02 11:48:37 -0400

    trip title edit

--- app/javascript/controllers/edit_day_controller.js
+++ app/javascript/controllers/edit_day_controller.js
@@ -1,6 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+	
+	setCursor(event) {
+		const inputElement = event.target
+		const inputLength = inputElement.value.length
+		
+		inputElement.setSelectionRange(inputLength, inputLength)
+	}
+	
 	submit(event) {
 		const form = event.target.form
 		form.requestSubmit()
