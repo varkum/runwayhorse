afea6e27ef67b21b559cff5d4f29a1818157d3c6
Varun Kumar <varun.kumar@yale.edu>
2024-04-09 11:02:30 -0400

    fix

--- app/controllers/trips/summaries_controller.rb
+++ app/controllers/trips/summaries_controller.rb
@@ -1,6 +1,4 @@
 class Trips::SummariesController < ApplicationController
-  skip_before_action :authenticate
-  
   def index
     @trip = Trip.find(params[:trip_id])
     @transport_happenings = @trip.transportations
