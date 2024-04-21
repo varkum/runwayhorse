c3453118bf267a758c613015b0568a5c7198703d
Varun Kumar <varun.kumar@yale.edu>
2024-04-21 16:08:36 -0400

    updates

---
+++ app/javascript/controllers/trip_validations_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+	static targets = [ "msg", "start", "end" ]
+	
+	validateDates() {
+		const startDate = new Date(this.startTarget.value)
+		const endDate = new Date(this.endTarget.value)
+		const threeMonthsLater = new Date(startDate.setMonth(startDate.getMonth() + 3))
+		
+		if (endDate > threeMonthsLater) {
+			this.msgTarget.style.display = "block"
+		} else {
+			this.msgTarget.style.display = "none"
+		}
+	}
+}
