d2954714ffd8ae4906c5e84d6406af90956017eb
Varun Kumar <varun.kumar@yale.edu>
2024-05-05 16:03:18 -0400

    hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "app.runwayhorse.com"
+  config.hosts << "runwayhorse.com"
+  
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
