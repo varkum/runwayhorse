640eb49b0236b0f8931650a6497cab8c84e5636a
Varun Kumar <varun.kumar@yale.edu>
2024-05-17 11:44:05 -0400

    share transportation

--- app/controllers/share/transportations_controller.rb
+++ app/controllers/share/transportations_controller.rb
@@ -1,2 +1,13 @@
-class Share::TransportationsController < ApplicationController
+class Share::TransportationsController < Share::TripsController
+  before_action :set_render_to_happenings
+  
+  def index
+    @happenings = @trip.transportations
+  end
+  
+  private
+  
+  def set_render_to_happenings
+    lookup_context.prefixes.prepend "share/happenings"
+  end
 end
