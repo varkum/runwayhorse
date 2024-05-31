afbd6bd44d5d81e8de40bc75eb1f2b61041baa17
Varun Kumar <varun.kumar@yale.edu>
2024-05-31 10:54:49 -0400

    lodging date fix

--- app/models/lodging.rb
+++ app/models/lodging.rb
@@ -2,6 +2,14 @@ class Lodging < ApplicationRecord
   belongs_to :trip, touch: true
   has_many :days, -> { order(date: :asc) }, dependent: :nullify
 
+  def start_date
+    days.first&.date
+  end
+  
+  def end_date
+    days.last&.date
+  end
+  
   def assign_days(from:, to:)
     lodging_days = trip.days.where(date: from..to)
     lodging_days.update!(lodging_id: id)
