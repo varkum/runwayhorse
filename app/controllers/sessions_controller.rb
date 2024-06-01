e6e196d459cabd61f0e1b251bde9a7d682788cd7
Varun Kumar <varun.kumar@yale.edu>
2024-06-01 09:32:26 -0400

    downcase signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_by(email: params[:email], password: params[:password])
+    if user = User.authenticate_by(email: params[:email].strip.downcase, password: params[:password])
       session[:user_id] = user.id
       redirect_to trips_path
     else
