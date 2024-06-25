659fa7673fb228abef65e04123040ccdccd68f22
Varun Kumar <varun.kumar@yale.edu>
2024-06-25 16:54:50 -0400

    see in google maps

--- app/helpers/lodgings_helper.rb
+++ app/helpers/lodgings_helper.rb
@@ -1,2 +1,7 @@
 module LodgingsHelper
+  def google_maps_link(text:, address:)
+    base_url = "https://www.google.com/maps/search/?api=1&query="
+    encoded_address = URI.encode_www_form_component(address)
+    link_to text, "#{base_url}#{encoded_address}", class: "text-blue-500 text-sm", target: "_blank"
+  end
 end
